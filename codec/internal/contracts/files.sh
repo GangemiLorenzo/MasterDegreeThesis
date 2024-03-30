e97abd66fea6f062eea991e77f73216388dc46f0
GangemiLorenzo <lorenzo.gangemi.developer@gmail.com>
2024-03-30 12:21:25 +0100

    chore: updare gitignore

--- codec/internal/contracts/files.sh
+++ codec/internal/contracts/files.sh
@@ -4,6 +4,6 @@ echo "const ("
 for file in files/*; do
     filename=$(basename "$file" .sol)
     identifier=$(echo "$filename" | tr '.' '_' | sed -r 's/(^|_)([a-z])/\U\2/g')
-    echo "    File$identifier FileIdentifier = \"codec/internal/contracts/files/$filename\""
+    echo "    File$identifier FileIdentifier = \"$filename\""
 done
 echo ")"
